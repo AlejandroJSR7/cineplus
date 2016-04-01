ec8243a8903e2d0821f5e46c6b42fa0d22ddac51
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2016-04-01 17:01:34 -0500

    cinePlus base

---
+++ js/cineplus.js
@@ -0,0 +1,28 @@
+console.log("cineplus.js");
+
+$('#slider-main').slick({
+  centerMode: false,
+  dots: true,
+  arrows: true,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
